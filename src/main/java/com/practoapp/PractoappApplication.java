627c1670d0d2cdf8dd813cfac3a1c151598bc132
gaurav <gauravjavare@gmail.com>
2024-01-16 16:14:28 +0530

    second commit

--- src/main/java/com/practoapp/PractoappApplication.java
+++ src/main/java/com/practoapp/PractoappApplication.java
@@ -11,7 +11,6 @@ public class PractoappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PractoappApplication.class, args);
-		System.out.println("hello");
 	}
 	@Bean
 	public PasswordEncoder passwordEncoder() {
